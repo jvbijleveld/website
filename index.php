11cd6663eed42bacb3f4a62648003f72b1266444
jvbijleveld <jeffrey@vanbijleveld.nl>
2017-02-25 12:35:28 +0100

    initial website

---
+++ index.php
@@ -0,0 +1,69 @@
+<html>
+	<head>
+		<title>vanBijleveld.nl</title>
+		<link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet"> 
+		<style>
+		
+			body{
+				font-family: 'Play', sans-serif;
+			}
+			
+			.card {
+				border: 1px solid grey;
+				padding: 35px;
+				width: 380px;
+				margin: auto;
+				height: 200px;
+				margin-top: 150px;
+			}
+			
+			.card div {
+				margin-top: 30px;
+			}
+			
+			.name {
+				font-size: 24px;
+				text-align: center;
+			}
+			
+			.function{
+				font-size: 18px;
+				text-align: center;
+			}
+			
+			.icons {
+				float: right;
+			}
+			
+			.icon {
+				width: 32px;
+				height: 32px;
+				float: right;
+				margin: 0px 5px;
+			}
+			
+			.icon img {
+				width: 32px;
+				height: 32px;
+			}
+				
+		
+		</style>
+	</head>
+	
+	<body>
+	
+	<div class="card">
+		<div class="name">Jeffrey van Bijleveld</div>
+		<div class="function">Application Developer</div>
+		
+		<div class="icons">
+			<div class="icon"><a href="https://github.com/jvbijleveld"><img src="./images/twitter.png"></a></div>
+			<div class="icon"><a href="https://github.com/jvbijleveld"><img src="./images/GitHub.png"></a></div>
+			<div class="icon"><a href="https://github.com/jvbijleveld"><img src="./images/linkedin.png"></a></div>
+			
+		</div>
+	</div>
+	
+	</body>
+</html>
